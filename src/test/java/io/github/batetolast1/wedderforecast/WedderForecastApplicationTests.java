902ef90f4fc384c1936942c76d750e8b422d43d5
batetolast1 <batetolast1@gmail.com>
2020-11-18 19:45:06 +0100

    initial project setup

---
+++ src/test/java/io/github/batetolast1/wedderforecast/WedderForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.batetolast1.wedderforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedderForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
