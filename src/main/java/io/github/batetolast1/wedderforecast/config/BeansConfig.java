900b1f691945da0ceebf854c7b952445e28fcd4c
batetolast1 <batetolast1@gmail.com>
2020-11-19 13:00:36 +0100

    added ModelMapper

---
+++ src/main/java/io/github/batetolast1/wedderforecast/config/BeansConfig.java
@@ -0,0 +1,17 @@
+package io.github.batetolast1.wedderforecast.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+}
