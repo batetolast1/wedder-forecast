c23bbda92afa15a2dd609eb100fae1de58e637c7
batetolast1 <batetolast1@gmail.com>
2020-11-29 18:33:08 +0100

    configured properties using env variables and Spring Configuration Processor

--- src/main/java/io/github/batetolast1/wedderforecast/WedderForecastApplication.java
+++ src/main/java/io/github/batetolast1/wedderforecast/WedderForecastApplication.java
@@ -2,8 +2,10 @@ package io.github.batetolast1.wedderforecast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("io.github.batetolast1.wedderforecast.config")
 public class WedderForecastApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/io/github/batetolast1/wedderforecast/config/property/WeatherSourceApiProperties.java
@@ -0,0 +1,30 @@
+package io.github.batetolast1.wedderforecast.config.property;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "api.weathersource")
+@ConstructorBinding
+
+@Getter
+public class WeatherSourceApiProperties {
+
+    private final String apiKey;
+    private final String host;
+    private final String path;
+    private final String timestampQuery;
+    private final String fieldsQuery;
+    private final String dailyFields;
+    private final String hourlyFields;
+
+    public WeatherSourceApiProperties(String apiKey, String host, String path, String timestampQuery, String fieldsQuery, String dailyFields, String hourlyFields) {
+        this.apiKey = apiKey;
+        this.host = host;
+        this.path = path;
+        this.timestampQuery = timestampQuery;
+        this.fieldsQuery = fieldsQuery;
+        this.dailyFields = dailyFields;
+        this.hourlyFields = hourlyFields;
+    }
+}
