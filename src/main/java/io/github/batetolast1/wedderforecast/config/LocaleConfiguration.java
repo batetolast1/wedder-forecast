4c5d0ec70207713e55fa1be6c5b99926d53a16ff
batetolast1 <batetolast1@gmail.com>
2020-12-06 19:33:46 +0100

    added application internationalisation feature

--- src/main/java/io/github/batetolast1/wedderforecast/config/LocaleConfiguration.java
+++ src/main/java/io/github/batetolast1/wedderforecast/config/LocaleConfiguration.java
@@ -1,15 +1,41 @@
 package io.github.batetolast1.wedderforecast.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @Configuration
-public class LocaleConfiguration {
+public class LocaleConfiguration implements WebMvcConfigurer {
 
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
