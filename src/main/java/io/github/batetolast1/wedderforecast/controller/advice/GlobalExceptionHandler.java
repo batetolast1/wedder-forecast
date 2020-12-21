8f1878da1c6ecf8434a097064777dac9bd4cc138
batetolast1 <batetolast1@gmail.com>
2020-12-21 18:35:33 +0100

    updated ExceptionInfo class with "exceptionClass" field

--- src/main/java/io/github/batetolast1/wedderforecast/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/io/github/batetolast1/wedderforecast/controller/advice/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package io.github.batetolast1.wedderforecast.controller.advice;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,6 +21,7 @@ public class GlobalExceptionHandler {
         log.warn("Exception caught: {}", exception.getMessage(), exception);
         ExceptionInfo exceptionInfo = new ExceptionInfo();
         exceptionInfo.setExceptionDateTime(LocalDateTime.now());
+        exceptionInfo.setExceptionClass(exception.getClass().getSimpleName());
         exceptionInfo.setMessage(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView("error/error-500");
@@ -26,10 +29,13 @@ public class GlobalExceptionHandler {
         return modelAndView;
     }
 
-    @Data
+    @NoArgsConstructor
+    @Getter
+    @Setter
     public static class ExceptionInfo {
 
         private LocalDateTime exceptionDateTime;
         private String message;
+        private String exceptionClass;
     }
 }
